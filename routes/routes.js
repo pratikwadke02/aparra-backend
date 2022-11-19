3f2f6948aef2b888edc0f41eabcb212eb856d41c
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-11-19 14:50:36 +0530

    server init

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+module.exports = (app) => {
+    const banner = require("../controllers/admin/banner.controller.js");
+    const category = require("../controllers/admin/category.controller.js");
+    const productImage = require("../controllers/admin/productImage.controller.js");
+    const testimonials = require("../controllers/admin/testimonials.controllers");
+  
+    const router = require("express").Router();
+  
+    router.post("/addBanner", banner.create);
+    router.get("/getBanner", banner.findAll);
+  
+    router.post("/addCategory", category.create);
+    router.get("/getCategory", category.findAll);
+  
+    router.post("/addModularKitchen", productImage.create);
+    router.get("/getModularKitchens", productImage.findAll);
+
+  
+    router.post("/addTestimonials", testimonials.create);
+    router.get("/getTestimonials", testimonials.findAll);
+  
+    app.use("/api/aparra", router);
+  };
+  
